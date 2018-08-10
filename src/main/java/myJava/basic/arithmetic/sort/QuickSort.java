46271fb45f736ff630c13fc7b8007660c35674fe
humengfei <humengfei001@ke.com>
2018-08-10 22:34:02 +0800

    QuickSort

---
+++ src/main/java/myJava/basic/arithmetic/sort/QuickSort.java
@@ -0,0 +1,59 @@
+package myJava.basic.arithmetic.sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    int[] arr;
+    QuickSort(int[] arr){
+        this.arr = arr;
+    }
+
+    public void swap(int li, int ri){
+        int tempValue = arr[li];
+        arr[li] = arr[ri];
+        arr[ri] = tempValue;
+    }
+
+    /**
+     * 时间复杂度：
+     * 最有情况下为O(nlogn)
+     * 最坏情况下为O(n^2)
+     *
+     * @param head
+     * @param tail
+     */
+    public void sort(int head, int tail){
+        if(arr==null || arr.length<=0 || head>=tail){
+            return;
+        }
+        int tempHead = head;
+        int tempTail = tail;
+        int pivot = arr[(head+tail)/2];
+        while(tempHead <= tempTail){
+            while(arr[tempHead] < pivot){
+                ++tempHead;
+            }
+            while(arr[tempTail] > pivot){
+                --tempTail;
+            }
+            if(tempHead < tempTail){
+                swap(tempHead, tempTail);
+                ++tempHead;
+                --tempTail;
+            }
+            if(tempHead == tempTail){
+                ++tempHead;
+            }
+        }
+
+        sort(head, tempTail);
+        sort(tempHead, tail);
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+        QuickSort quickSort = new QuickSort(arr);
+        quickSort.sort(0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
