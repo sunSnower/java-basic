763b4e209f1841d472199892d021aa58a478fc83
humengfei <humengfei001@ke.com>
2018-08-11 15:07:15 +0800

    mergeSort

--- src/main/java/myJava/basic/arithmetic/sort/MergeSort.java
+++ src/main/java/myJava/basic/arithmetic/sort/MergeSort.java
@@ -9,27 +9,7 @@ public class MergeSort {
     MergeSort(int[] arr){
         this.arr = arr;
     }
-
-    public void mergeSort(){
-
-    }
-    public void mergeArr(int[] fragement1, int[] fragement2, int[] merge){
-        int index1 = 0;
-        int index2 = 0;
-        int index3 = 0;
-        if(fragement1==null || fragement1.length<1 || fragement1==null || fragement2.length<1){
-            return;
-        }
-        while(index1<fragement1.length && index2<fragement2.length){
-            while(fragement1[index1] <= fragement2[index2]){
-                merge[index3++] = fragement1[index1++];
-            }
-            while(fragement2[index2] < fragement1[index1]){
-                merge[index3++] = fragement2[index2++];
-            }
-        }
-    }
-
+    
     /**
      * 归并排序
      * 是分治法的一种典型应用，且各层可以分开进行
