f3d9e9c45630ab0fe2c59f0e2aa9732084ebb908
humengfei <humengfei001@ke.com>
2018-08-13 10:50:41 +0800

    TopK

---
+++ src/main/java/arithmetic/test/KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,15 @@
+package arithmetic.test;
+
+public class KthSmallestElementInASortedMatrix {
+    int[][] matrix;
+    KthSmallestElementInASortedMatrix(int[][] matrix){
+        this.matrix = matrix;
+    }
+
+    public void resort(){
+
+    }
+    public int getKthSmallestElement(int k){
+        return 0;
+    }
+}
