78bec7e6cedc8b2b99450b33896c3329bfc23b89
humengfei <humengfei001@ke.com>
2018-08-13 16:04:08 +0800

    mysql

---
+++ src/main/java/arithmetic/test/NumberConvert.java
@@ -0,0 +1,18 @@
+package arithmetic.test;
+
+public class NumberConvert {
+    public static final String A = "10";
+    public static final String B = "11";
+    public static final String C = "12";
+    public static final String D = "13";
+    public static final String E = "14";
+    public static final String F = "15";
+
+    public String convert(int n){
+        String reslut = "";
+        while(n/16 > 0){
+
+        }
+        return null;
+    }
+}
